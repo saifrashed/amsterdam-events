dac64147f02a9bb3972bb98be66139ae9bf9f013
Saif <saifeddinerashed@icloud.com>
2021-09-09 00:08:54 +0200

    init project

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/mainpage/header/header.component';
+import { HomeComponent } from './components/mainpage/home/home.component';
+import { NavBarComponent } from './components/mainpage/nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
