2bfffa24ef562a8bb989ceb8d7b489b3c5f08887
Saif Rashed <saif.rashed@hva.nl>
2021-10-24 23:28:08 +0200

    crud fix

--- src/app/aeserver/src/main/java/com/aeserver/repository/AEventsRepository.java
+++ src/app/aeserver/src/main/java/com/aeserver/repository/AEventsRepository.java
@@ -1,8 +1,10 @@
 package com.aeserver.repository;
 
+
 import com.aeserver.model.AEvent;
 
 import java.util.List;
 
 public interface AEventsRepository  {
+  public List<AEvent> findAll();
 }
--- src/app/aeserver/src/main/java/com/aeserver/repository/AEventsRepositoryMock.java
+++ src/app/aeserver/src/main/java/com/aeserver/repository/AEventsRepositoryMock.java
@@ -21,29 +21,6 @@ public class AEventsRepositoryMock implements AEventsRepository{
     events.add(AEvent.createRandomAEvent());
   }
 
-  List<AEvent> findAll(){
-    AEventsRepositoryMock.findAll();
-  };
-
-  AEvent findById(long id){
-
-  }
-    return AEventsRepositoryMock.findById(id)
-  };
-
-  @Transactional
-  AEvent save(AEvent aEvent){
-    if (AEventsRepositoryMock.isNew(aEvent)) {
-      em.persist(aEvent);
-      return aEvent;
-    } else {
-      return em.merge(aEvent);
-    };
-
-  boolean deleteById(long id){
-
-  };
-
   @Override
   public List<AEvent> findAll() {
     return events;
